c7a0f923a83e7462664144474ae8b9c417bc0bb2
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-25 12:13:43 -0300

    Estruturas Sequênciais - ex012

--- pythonProjectExercicios/Estruturas Sequenciais/ex012.py
+++ pythonProjectExercicios/Estruturas Sequenciais/ex012.py
@@ -2,3 +2,8 @@
 
 nome = input("Qual é o seu nome? ")
 print(f"Boa tarde, {nome}!")
+
+#12.1 Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Seu nome: ')
+print(f'Olá {nome}! Prazer em te conhecer!')
