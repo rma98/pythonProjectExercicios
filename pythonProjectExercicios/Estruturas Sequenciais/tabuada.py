bdcd91ed8dd71e2e1488761c1ecaba0db3370eef
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-25 11:14:29 -0300

    Estruturas Sequênciais - tabuada

---
+++ pythonProjectExercicios/Estruturas Sequenciais/tabuada.py
@@ -0,0 +1,20 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+num = int(input('Tabuada do número: '))
+print('='*12)
+print(f'{num} x 1 = {num * 1}\n'
+      f'{num} x 2 = {num * 2}\n'
+      f'{num} x 3 = {num * 3}\n'
+      f'{num} x 4 = {num * 4}\n'
+      f'{num} x 5 = {num * 5}\n'
+      f'{num} x 6 = {num * 6}\n'
+      f'{num} x 7 = {num * 7}\n'
+      f'{num} x 8 = {num * 8}\n'
+      f'{num} x 9 = {num * 9}\n'
+      f'{num} x 10 = {num * 10}')
+print('='*12)
+
+#numero = int(input('Número: '))
+#tabuada = 0
+#for tab in range(tabuada+1, 11):
+#    print(f'{tabuada+1} x {numero} = {(tabuada +1) * numero}')
