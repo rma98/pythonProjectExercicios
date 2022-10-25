c6d78c27d0d6a39d4b97d89ba79c9208f4d7e632
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-25 08:01:31 -0300

    Estruturas Sequênciais - ex00

--- pythonProjectExercicios/Estruturas Sequenciais/ex00.py
+++ pythonProjectExercicios/Estruturas Sequenciais/ex00.py
@@ -6,3 +6,17 @@ print('Olá, Mundo!')
 
 nome = input('Seu nome: ')
 print(f'Olá {nome}! Prazer em te conhecer!')
+
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+print(f'{num1} + {num2} = {num1 + num2}')
+
+# Faça um programa que leia algo pelo usuário e mostre na tela seu tipo primitivo e todas as informações possíveis sobre
+# ela.
+
+infor = input('Digite algo: ')
+print(f'Tipo primitivo: {type(infor)}\nSó tem espaços? {infor.isspace()}\nÉ um número? {infor.isnumeric()}'
+      f'\nÉ alfabético? {infor.isalpha()}\nÉ alfanumérico? {infor.isalnum()}\nEstá em maiúsculos? {infor.isupper()}'
+      f'\nEstá em minúsculos? {infor.islower()}\nEstá capitalizada? {infor.istitle()}')
