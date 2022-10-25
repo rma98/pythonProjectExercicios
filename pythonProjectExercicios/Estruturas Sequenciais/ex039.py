e4c9cbac115205bdabb3fd88a916437355352b02
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-25 10:08:34 -0300

    Estruturas Sequênciais - ex039

--- pythonProjectExercicios/Estruturas Sequenciais/ex039.py
+++ pythonProjectExercicios/Estruturas Sequenciais/ex039.py
@@ -4,3 +4,9 @@ n1 = int(input("Primeiro número: "))
 n2 = int(input("Segundo número: "))
 n3 = int(input("Terceiro número: "))
 print(f"{n1} x {n2} x {n3} = {n1 * n2 * n3}")
+
+#39.1 Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+numero = int(input('Número: '))
+print(f'Dobro: {numero *2}\nTriplo: {numero *3}\nRaiz quadrada: {numero ** (1/2):.2f}\n'
+      f'Outra maneira de se calcular a raiz qaudrada usando a função pow(): {pow(numero, (1/2)):.2f}')
