680a79d01dff82222c4a511f55174b1a11d2bcba
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-25 06:57:31 -0300

    Estruturas Sequênciais - ex00

--- pythonProjectExercicios/Estruturas Sequenciais/ex00.py
+++ pythonProjectExercicios/Estruturas Sequenciais/ex00.py
@@ -1,3 +1,8 @@
 # Crie um programa que escreva "Olá, Mundo!" na tela.
 
 print('Olá, Mundo!')
+
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Seu nome: ')
+print(f'Olá {nome}! Prazer em te conhecer!')
