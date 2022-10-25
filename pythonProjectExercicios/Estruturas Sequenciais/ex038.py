82b4f4236daabbbddca240db5a0b3a177de6266e
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-25 09:57:28 -0300

    Estruturas Sequênciais - ex038

--- pythonProjectExercicios/Estruturas Sequenciais/ex038.py
+++ pythonProjectExercicios/Estruturas Sequenciais/ex038.py
@@ -1,4 +1,9 @@
-#Faça um programa que receba dois números, calcule e mostre a subtração do primeiro pelo segundo
+#38 Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+num = int(input('Número: '))
+print(f'Antecessor: {num-1}\nSucessor: {num+1}')
+
+#38.1 Faça um programa que receba dois números, calcule e mostre a subtração do primeiro pelo segundo
 
 n1 = int(input("Primeiro número: "))
 n2 = int(input("Segundo número: "))
