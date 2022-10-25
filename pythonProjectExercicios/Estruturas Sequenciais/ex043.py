5184883e1c37945bd1ef6d7066449c3797a1b9c8
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-25 11:23:33 -0300

    Estruturas Sequênciais - ex043

--- pythonProjectExercicios/Estruturas Sequenciais/ex043.py
+++ pythonProjectExercicios/Estruturas Sequenciais/ex043.py
@@ -2,3 +2,9 @@
 
 peso = int(input("Peso: "))
 print(f"{peso}kg convertido em gramas é: {peso * 1000}g")
+
+#43.1 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
+# Considere US$ = 3,27
+
+dinheiro_na_carteira = float(input('Quanto dinheiro você tem na carteira? R$'))
+print(f'Você pode comprar US${dinheiro_na_carteira / 3.27:.2f} Dólares.')
