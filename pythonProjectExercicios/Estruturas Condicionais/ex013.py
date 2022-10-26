6205d2c79cc5110c5c0d63b0bf90ff7b89d7ccab
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-26 00:40:37 -0300

    Estruturas Condicionais - ex013

--- pythonProjectExercicios/Estruturas Condicionais/ex013.py
+++ pythonProjectExercicios/Estruturas Condicionais/ex013.py
@@ -9,3 +9,35 @@ if a > b + c and c < a - b:
     print(f"Prdouto: {a * b * c}")
 else:
     print(f"A ** C + B = {a ** c + b}")
+
+#13.1 Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+n3 = int(input('Terceiro número: '))
+if n1 > n2 and n1 > n3:
+    print(f'Maior: {n1}')
+elif n2 > n1 and n2 > n3:
+    print(f'Maior: {n2}')
+else:
+    print(f'Maior: {n3}')
+if n1 < n2 and n1 < n3:
+    print(f'Menor: {n1}')
+elif n2 < n1 and n2 < n3:
+    print(f'Menor: {n2}')
+else:
+    print(f'Menor: {n3}')
+#VERIFICANDO QUEM É MENOR
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+elif n3 < n1 and n3 < n2:
+    menor = n3
+#VERIFICANDO QUEM É MAIOR
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+elif n3 > n1 and n3 > n2:
+    maior = n3
+print(f'O menor valor digitado foi {menor}')
+print(f'O maior valor digitado foi {maior}')
