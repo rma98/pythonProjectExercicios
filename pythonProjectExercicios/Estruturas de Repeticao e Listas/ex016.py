b5666bf5f8c0358146c78b0d19a59423794d7c2d
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-10-28 17:18:28 -0300

    Estruturas de Repetição e Listas - ex016

---
+++ pythonProjectExercicios/Estruturas de Repeticao e Listas/ex016.py
@@ -0,0 +1,13 @@
+#16. Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar
+# o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles
+# (desconsiderando o flag)
+
+
+
+n = cont = soma = 0
+n = int(input('Número [999 para parar]:'))
+while n != 999:
+    soma += n
+    cont += 1
+    n = int(input('Número [999 para parar]:'))
+print(f'Você digitou {cont} números e a soma entre eles foi {soma}.')
