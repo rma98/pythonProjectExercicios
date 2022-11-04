a6df4eb9d152fab46e0b50ff0d5f25cbf39f4aa3
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-11-04 17:13:47 -0300

    Coleções - ex018

---
+++ pythonProjectExercicios/Colecoes/ex018.py
@@ -0,0 +1,16 @@
+#18. Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre
+# o conteúdo da estrutura na tela.
+
+aluno = {}
+
+aluno['nome'] = input('Nome: ')
+aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['média'] < 7:
+    aluno['situacão'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f' - {k} é igual a {v}')
