837ea3949ae98ead8dd94a2e17a098f0f5f8b160
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-11-05 21:59:25 -0300

    Funções - ex010

---
+++ pythonProjectExercicios/Funcoes/ex010.py
@@ -0,0 +1,34 @@
+#10. Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário
+# com as seguintes informações:
+# - Quantidade de notas
+# - A maior nota
+# - A menor nota
+# - A média da turma
+# - A situação (opcional)
+# Adicione também as docstrings da função.
+
+def notas(* n, sit=False):
+    '''
+    -> Função para análisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita várias)
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação.
+    :return: dicionário com várias sobre a situação da turma.
+    '''
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['média'] = sum(n)/len(n)
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+res = notas(5.5, 2.5, 10, 10, sit=True)
+print(res)
+help(notas)
