6ca79e92528211e57f63a72872d482099c7e1692
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-03-27 14:52:35 -0300

    Mensagem de saída Python #1

---
+++ pythonProjectExercicios/Estruturas Sequenciais/mensagem_de_saida.py
@@ -0,0 +1,21 @@
+# Crie um programa que escreva "Olá, Mundo!" na tela.
+
+print('Olá, Mundo!')
+
+# Outras formas de fazer
+
+# 1
+print("Olá, Mundo!")
+
+# 2
+mensagem = "Olá, Mundo!"
+print(mensagem)
+
+# 3
+print("Olá" + ", " + "Mundo" + "!")
+
+# 4
+print(f"Olá, {'Mundo'}!")
+
+# 5
+print("Olá, {}!".format("Mundo"))
