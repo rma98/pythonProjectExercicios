287b34978e4657ea387dbacb8489fcc1b4a78b9a
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-03-28 00:27:21 -0300

    Del #05

--- pythonProjectExercicios/Estruturas Sequenciais/ex005.py
+++
@@ -1,6 +0,0 @@
-#5. Escreva um programa que calcule o tempo de uma de carro. Pergunte a distância a pecorrer e a velocidade média
-# esperada para a viagem.
-
-distanica_Pecorrer = float(input("Distância a pecorrer: "))
-velocidade_Media = float(input("Velocidade Média: "))
-print(f"Tempo da viagem: {distanica_Pecorrer/velocidade_Media}")
