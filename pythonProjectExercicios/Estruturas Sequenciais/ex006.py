0a7e69878c4163a6a4c0815bb73f373c192e65b8
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-03-28 14:35:47 -0300

    Del #06

--- pythonProjectExercicios/Estruturas Sequenciais/ex006.py
+++
@@ -1,9 +0,0 @@
-#6. Pergunte dois valores ao usuário e imprima se o primeiro valor digitado é maior que o segundo.
-
-valor1 = int(input("Valor 1:"))
-valor2 = int(input("Valor 2:"))
-print("O primeiro valor digitado é maior que o segundo: ", end='')
-if valor1 > valor2:
-    print(True)
-else:
-    print(False)
