9e159e137795e6ccfb95b873fe4902de02e659b6
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-02 17:25:15 -0300

    Del #08

--- pythonProjectExercicios/Estruturas Sequenciais/ex008.py
+++
@@ -1,8 +0,0 @@
-#8. Criar um algoritmo que pergunta dois números ao usuário e informa a soma deles é maior que a subtração deles.
-
-valor1 = int(input("Valor 1: "))
-valor2 = int(input("Valor 2: "))
-if valor1 + valor2 > valor1 - valor2:
-    print(True)
-else:
-    print(False)
