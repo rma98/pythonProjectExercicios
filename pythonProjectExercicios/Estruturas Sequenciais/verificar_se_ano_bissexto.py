adef7362fb61ea5466f87ecfd7cb2a5fa7e2f8a1
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-03 18:48:26 -0300

    Verificar se ano bissexto #29

---
+++ pythonProjectExercicios/Estruturas Sequenciais/verificar_se_ano_bissexto.py
@@ -0,0 +1,19 @@
+# Imprima o número de dias de um ano. Lembra-se de verificar os anos bissextos.
+
+ano = int(input("Ano: "))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f"O ano de {ano} possui 366 dias.")
+else:
+    print(f"O ano de {ano} possui 365 dias.")
+
+# Outras formas de fazer
+
+# 1
+import calendar
+
+ano = int(input("Ano: "))
+
+if calendar.isleap(ano):
+    print(f"O ano de {ano} possui 366 dias.")
+else:
+    print(f"O ano de {ano} possui 365 dias.")
