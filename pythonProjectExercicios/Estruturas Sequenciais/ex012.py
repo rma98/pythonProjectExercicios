2e27e00c8c727d5f54176740456c9a344472591d
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-03 00:24:41 -0300

    Del #12

--- pythonProjectExercicios/Estruturas Sequenciais/ex012.py
+++
@@ -1,9 +0,0 @@
-#12. Perguntar o nome do usuário e dar "Boa tarde" a essa pessoa.
-
-nome = input("Qual é o seu nome? ")
-print(f"Boa tarde, {nome}!")
-
-#12.1 Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
-
-nome = input('Seu nome: ')
-print(f'Olá {nome}! Prazer em te conhecer!')
