27bcfb43fbb9d0cc23eb40981e530a8bf8ae753e
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-03 18:48:33 -0300

    Del #22

--- pythonProjectExercicios/Estruturas Sequenciais/ex022.py
+++
@@ -1,7 +0,0 @@
-#22. Imprima o número de dias de um ano. Lembra-se de verificar os anos bissextos.
-
-ano = int(input("Ano: "))
-if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-    print(f"O ano de {ano} possui 366 dias.")
-else:
-    print(f"O ano de {ano} possui 365 dias.")
