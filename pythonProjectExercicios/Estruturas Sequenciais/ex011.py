9be2de840419d516fb56e34797c35c89192498b3
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-02 22:40:20 -0300

    Del #11

--- pythonProjectExercicios/Estruturas Sequenciais/ex011.py
+++
@@ -1,7 +0,0 @@
-#11. Criar um algoritmo que pergunta um número ao usuário e imprime se esse número é múltilplo de 7 e de 3 ao mesmo tempo.
-
-numero = int(input("Número: "))
-if numero % 7 == 0 and numero % 3 == 0:
-    print(True)
-else:
-    print(False)
