4b820e363924c2c9822dcbf9d8ed6deaaad03e29
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-03 18:36:51 -0300

    Del #21

--- pythonProjectExercicios/Estruturas Sequenciais/ex021.py
+++
@@ -1,7 +0,0 @@
-#21. Leia uma letra do alfabeto e mostrar uma mensagem: se é vogal ou consoante.
-
-letra_Alfabeto = input("Letra do alfabeto: ").upper()
-if letra_Alfabeto == "A" or letra_Alfabeto == "E" or letra_Alfabeto == "I" or letra_Alfabeto == "O" or letra_Alfabeto == "U":
-    print("Vogal")
-else:
-    print("Consoante")
