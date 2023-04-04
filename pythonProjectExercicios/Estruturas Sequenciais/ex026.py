6bbfa4419c3270be245a1b420e4760ba65e5c67f
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-03 22:26:30 -0300

    Del #26

--- pythonProjectExercicios/Estruturas Sequenciais/ex026.py
+++
@@ -1,5 +0,0 @@
-#26. Pergunte ao usuário duas palavras, depois imprima as palavras na ordem contrária â alfabética.
-
-palavra1 = input("Primeira palavra: ")
-palavra2 = input("Segunda palavra: ")
-print(f"Ordem contrária:\n{palavra1[::-1]}\n{palavra2[::-1]}")
