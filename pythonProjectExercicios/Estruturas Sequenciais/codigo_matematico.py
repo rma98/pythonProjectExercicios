96bc997ec9a334238cc6d1858bac516cbb8e57c3
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-05 14:56:40 -0300

    Código Matemático #37

---
+++ pythonProjectExercicios/Estruturas Sequenciais/codigo_matematico.py
@@ -0,0 +1,20 @@
+# Pergunte ao usuário três números (A, B, C). Se o primeiro for maior que a soma dos outros dois e o terceiro for
+# menor que a subtração entre A e B, deve imprimir o produto entre os 3 valores caso contrário deve imprimir o seguinte
+# cálculo A**C+B.
+
+a = int(input("A: "))
+b = int(input("B: "))
+c = int(input("C: "))
+if a > b + c and c < a - b:
+    print(f"Produto: {a * b * c}")
+else:
+    print(a**c+b)
+
+# Outras formas de fazer
+
+# 1
+a = int(input("A: "))
+b = int(input("B: "))
+c = int(input("C: "))
+
+print(f"Produto: {a * b * c}" if a > (b + c) and c < (a - b) else a ** c + b)
