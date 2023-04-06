7cd774b0a0bd7856ef13990675e292a41d6e38bb
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-06 14:50:56 -0300

    Del #50

--- pythonProjectExercicios/Estruturas Sequenciais/ex050.py
+++
@@ -1,5 +0,0 @@
-# Faça um programa que calcule e mostre a área de um losango. Sabe-se que: Área = (diagonal maior * diagonal menor) / 2
-
-diagonal_Maior = int(input("Diagonal Maior: "))
-diagonal_Menor = int(input("Diagonal Menor: "))
-print(f"Área do losango: {diagonal_Maior * diagonal_Menor / 2}")
