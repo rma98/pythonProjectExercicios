d02c705ce5193b03d6123ffbb8a959139146bb3b
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-06 13:54:27 -0300

    Del #45

--- pythonProjectExercicios/Estruturas Sequenciais/ex045.py
+++
@@ -1,6 +0,0 @@
-#45. Faça um programa que recebe o valor do salário mínimo e o valor do salário de um funcionário, calcule e mostre a
-# quantidade de salários mínimos que esse funcionário ganha.
-
-salario_Minino = float(input("Salário Mínimo: R$"))
-salario_Funcionario = float(input("Salário do Funcionário: R$"))
-print(f"Quantidade de salários mínimos: R${salario_Funcionario / salario_Minino:.2f}")
