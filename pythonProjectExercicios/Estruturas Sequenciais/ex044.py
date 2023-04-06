f22269f3a112e7876eef95535b30f77a810f1f6b
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-06 13:36:55 -0300

    Del #44

--- pythonProjectExercicios/Estruturas Sequenciais/ex044.py
+++
@@ -1,6 +0,0 @@
-#44. Faça um programa que calcule e mostre a área de um trapézio. Sabe-se que: Área = ((base maior + base menor) * altura) / 2
-
-base_Maior = int(input("Base Maior: "))
-base_Menor = int(input("Base Menor: "))
-altura = int(input("Altura: "))
-print(f"A área do trapézio é: {((base_Maior + base_Menor) * altura) / 2}")
