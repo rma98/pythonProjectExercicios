8291c9a4911092517fe0d61c9ce78672b526421b
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-06 14:38:54 -0300

    Del #49

--- pythonProjectExercicios/Estruturas Sequenciais/ex049.py
+++
@@ -1,8 +0,0 @@
-# Faça um programa que receba o peso de uma pessoa, calcule e mostre:
-# -> o novo peso, se a pessoa engordar 15% sobre o peso digitado.
-# -> o novo peso, se a pessoa emagrecer 20% sobre o peso digitado.
-
-peso = float(input("Peso: "))
-aumento = peso * 15/100
-perca = peso * 20/100
-print(f"Se engordou: {peso + aumento}kg\nSe emagreceu: {peso - perca}kg")
