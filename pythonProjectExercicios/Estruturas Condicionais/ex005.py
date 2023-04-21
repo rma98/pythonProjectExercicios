f6634feba9cc9fad90c15f056e186f7c92eee1aa
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-21 16:35:31 -0300

    Del #70

--- pythonProjectExercicios/Estruturas Condicionais/ex005.py
+++
@@ -1,7 +0,0 @@
-#5. Leia uma letra do alfabeto e mostrar uma mensagem: se é vogal ou consoante.
-
-alfabeto = input("Alfabeto: ").upper()
-if alfabeto == "A" or alfabeto == "E" or alfabeto == "I" or alfabeto == "O" or alfabeto == "U":
-    print("Vogal!")
-else:
-    print("Alfabeto!")
