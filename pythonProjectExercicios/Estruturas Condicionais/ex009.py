e8936ecffb9d540f26a5d7ea2936a097a6821f7e
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-04-21 17:29:55 -0300

    Del #74

--- pythonProjectExercicios/Estruturas Condicionais/ex009.py
+++
@@ -1,8 +0,0 @@
-#9. Pergunte ao usuário duas palavras, depois imprima as palavras na ordem contrária à alfabética.
-
-palavra1 = input("Primeira palavra: ")
-palavra2 = input("Segunda palavra: ")
-juncao = palavra1 + " " + palavra2
-print(juncao)
-ordem_Contraria = juncao[::-1]
-print(f"Ordem contrária à alfabética: {ordem_Contraria}")
